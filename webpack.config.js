feee7d29489b0df0f5a6232b8184ad0b978cc405
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-09-07 16:35:14 -0300

    adjust configs

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
       filename: "remoteEntry.js",
       remotes: {
         componentsLibrary:
-          "components_library@https://components-library-eosin.vercel.app/remoteEntry.js",
+          "components_library@https://components-library-git-develop-dev-mauricioab.vercel.app/remoteEntry.js",
       },
       exposes: {
         "./AppA": "./src/App.tsx",
